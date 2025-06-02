bab844ef5b164b36089992253512fd47703af3f7
tae.y <0211ilyoil@gmail.com>
2025-06-02 15:57:22 +0900

    feat 2-5

---
+++ coreJavaScript/2-5/number.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//alert(1 / 0); // 무한대
+
+const bigInt = 1234567890123456789012345678901234567890n;
+
+let name = "Ilya";
+
+alert(`hello ${1}`); // ?
+
+alert(`hello ${"name"}`); // ?
+
+alert(`hello ${name}`); // ?

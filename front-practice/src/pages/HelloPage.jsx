7229eddff26e35cfdac97fe808db067e26d94b9d
ilyoil2 <0211ilyoil@gmail.com>
2025-07-29 14:43:24 +0900

    feat :: 색 설정

--- front-practice/src/pages/HelloPage.jsx
+++ front-practice/src/pages/HelloPage.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
-import styled from 'styled-components';
+import React, { useState } from "react"; 
+import styled from 'styled-components'; 
 
 const LargeText = styled.h1`
   font-size: 4rem;
   text-align: center;
   margin-top: 20vh;
+  color: ${({ theme }) => theme.colors.gray500};
 `;
 
 const HelloPage = () => {
